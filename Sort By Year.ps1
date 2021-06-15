f731207b0c99987d256df2b3d392e19c8a8cf25b
Sénéchal Julien <35048771+Teckinfor@users.noreply.github.com>
2021-06-15 14:55:09 +0200

    Mp4 and Jpeg added

--- Sort By Year.ps1
+++ Sort By Year.ps1
@@ -2,9 +2,9 @@ $path = Read-Host -Prompt "Path";
 
 $allFilesJpg = dir $path *.jpg;
 $allFilesPng = dir $path *.png;
+$allFilesMp4 = dir $path *.mp4;
+$allFilesJpeg = dir $path *.jpeg;
 
-
-#Jpeg files
 foreach ($pic in $allFilesJpg) {
 
     $date = (echo $pic | select LastWriteTime);
@@ -20,9 +20,27 @@ foreach ($pic in $allFilesJpg) {
         Move-Item $pic.PSPath $CompletePath
     }
 }
+
+Write-Host "Jpg - Finished";
+
+foreach ($pic in $allFilesJpeg) {
+
+    $date = (echo $pic | select LastWriteTime);
+    $year = $date.LastWriteTime.Year;
+    $CompletePath = $path + "\" + $year
+
+    $RepoYearExist = Test-Path $path"\"$year;
+
+    if (!$RepoYearExist){
+        mkdir $path"\"$year | Out-Null;
+        Move-Item $pic.PSPath $CompletePath
+    } else {
+        Move-Item $pic.PSPath $CompletePath
+    }
+}
+
 Write-Host "Jpeg - Finished";
 
-#Png Files
 foreach ($pic in $allFilesPng) {
 
     $date = (echo $pic | select LastWriteTime);
@@ -38,4 +56,23 @@ foreach ($pic in $allFilesPng) {
         Move-Item $pic.PSPath $CompletePath
     }
 }
+
 Write-Host "Png - Finished";
+
+foreach ($pic in $allFilesMp4) {
+
+    $date = (echo $pic | select LastWriteTime);
+    $year = $date.LastWriteTime.Year;
+    $CompletePath = $path + "\" + $year
+
+    $RepoYearExist = Test-Path $path"\"$year;
+
+    if (!$RepoYearExist){
+        mkdir $path"\"$year | Out-Null;
+        Move-Item $pic.PSPath $CompletePath
+    } else {
+        Move-Item $pic.PSPath $CompletePath
+    }
+}
+
+Write-Host "Mp4 - Finished";
