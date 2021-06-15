ec3d264ceeb8ccf94aac8e25f8b7b1ede09ecc94
Sénéchal Julien <35048771+Teckinfor@users.noreply.github.com>
2021-06-15 14:49:06 +0200

    Add files via upload

---
+++ Sort By Year.ps1
@@ -0,0 +1,45 @@
+﻿$path = Read-Host -Prompt "Path";
+
+$allFilesJpg = dir $path *.jpg;
+$allFilesPng = dir $path *.png;
+
+foreach ($pic in $allFilesJpg) {
+
+    $date = (echo $pic | select LastWriteTime);
+    $year = $date.LastWriteTime.Year;
+    $CompletePath = $path + "\" + $year
+
+    $RepoYearExist = Test-Path $path"\"$year;
+
+    if (!$RepoYearExist){
+        mkdir $path"\"$year | Out-Null;
+        Move-Item $pic.PSPath $CompletePath
+    } else {
+        Move-Item $pic.PSPath $CompletePath
+    }
+
+    
+
+}
+
+Write-Host "Jpeg - Finished";
+
+foreach ($pic in $allFilesPng) {
+
+    $date = (echo $pic | select LastWriteTime);
+    $year = $date.LastWriteTime.Year;
+    $CompletePath = $path + "\" + $year
+
+    $RepoYearExist = Test-Path $path"\"$year;
+
+    if (!$RepoYearExist){
+        mkdir $path"\"$year | Out-Null;
+        Move-Item $pic.PSPath $CompletePath
+    } else {
+        Move-Item $pic.PSPath $CompletePath
+    }
+
+    
+}
+
+Write-Host "Png - Finished";
